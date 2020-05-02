9eb78da8a0eebed8d6c3871769f1ae05d0b6f3a1
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-05-02 07:51:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DomContentLoaded", function(){
   let stuff = document.getElementById('text');
-  stuff.innerHtml = 
+  stuff.innerHtml = "This is really cool!"
 })
